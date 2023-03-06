942f64c217dc6907059b163d584bae8920878601
Aya Mohammed <aya59268@gmail.com>
2023-03-06 02:05:29 +0200

    ADD Files

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+   
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+              <img src="https://cdn.changelog.com/uploads/icons/news_sources/Qo/icon_large.png?v=63680075631" width="250" height="250" class="rounded-full">
+            </div>
+            
+            <div class="col-9">
+
+            </div>
+        </div>
+    </div>
+ 
+</x-app-layout>
